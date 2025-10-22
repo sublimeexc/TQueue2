32993294db9bb5a9624f85b21fb2cc62774f7351
sublimeexc <gangarh@mail.ru>
2025-10-22 15:36:51 +0300

    adding include

---
+++ include/tqueue.h
@@ -0,0 +1,165 @@
+﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// Copyright (c) Сысоев А.В.
+//
+//
+
+#ifndef __TQueue_H__
+#define __TQueue_H__
+
+#include <iostream>
+
+// стек
+
+template<class T>
+class TStack
+{
+protected:
+    size_t sz;
+    T* pMem;
+    int top;
+public:
+    TStack();
+    TStack(size_t size);
+    TStack(const TStack& s);
+    TStack(TStack&& s);
+    ~TStack();
+    void Push(T elem);
+    T Pop();
+    TStack& operator=(TStack& s);
+    bool operator==(const TStack s);
+    bool operator!=(const TStack s);
+    bool IsFull();
+    bool IsEmpty();
+    void Resize(size_t newsize);
+};
+
+template<class T>
+TStack<T>::TStack()
+{
+    sz = 0;
+    pMem = nullptr;
+    top = -1;
+}
+
+template<class T>
+TStack<T>::TStack(size_t size)
+{
+    sz = size;
+    pMem = new T[size];
+    top = 0;
+}
+
+template<class T>
+TStack<T>::TStack(const TStack& s)
+{
+    sz = s.sz;
+    pMem = new T[size];
+    if (pMem != nullptr)
+    {
+        for (int i = 0; i < sz; i++)
+        {
+            pMem[i] = s.pMem[i];
+        }
+    }
+    top = s.top;
+}
+
+template<class T>
+TStack<T>::TStack(TStack&& s)
+{
+    sz = s.sz;
+    pMem = s.pMem;
+    s.pMem = nullptr;
+    s.sz = 0;
+}
+
+template<class T>
+TStack<T>::~TStack()
+{
+    sz = 0;
+    if (pMem != nullptr)
+    {
+        delete[] pMem;
+        pMem = nullptr;
+    }
+    top = -1;
+}
+
+template<class T>
+void TStack<T>::Push(T elem)
+{
+    if (isFull() || pMem == nullptr) throw std::exception();
+    pMem(top) = elem;
+    top++;
+}
+template<class T>
+T TStack<T>::Pop()
+{
+    if (isEmpty() || pMem == nullptr) throw std::exception();
+    T res = pMem(top);
+    pMem(top) = 0;
+    top--;
+    return res;
+}
+
+template<class T>
+TStack<T>& TStack<T>::operator=(TStack& s)
+{
+    if (this != &s)
+    {
+        top = s.top;
+        sz = s.sz;
+        if (sz == 0)
+        {
+            pMem = nullptr;
+        }
+        else 
+        {
+            pMem = new T[size];
+            for (int i = 0; i < sz; i++)
+            {
+                pMem[i] = s.pMem[i];
+            }
+        }
+    }
+    return *this;
+}
+
+template<class T>
+bool TStack<T>::operator==(const TStack s)
+{
+    return (sz == s.sz && pMem == s.pMem && top == s.top);
+}
+
+template<class T>
+bool TStack<T>::operator!=(const TStack s)
+{
+    return !(this == s);
+}
+
+// очередь
+
+template<class T>
+class TQueue
+{
+protected:
+    size_t sz;
+    T* pMem;
+    T* head;
+    T* tail;
+public:
+    TQueue();
+    TQueue();
+    TQueue();
+    TQueue();
+    TQueue();
+    TQueue();
+    void Push(T elem);
+    void Pop(T elem);
+    TQueue& operator=(TQueue& q);
+    bool operator==(const TQueue q);
+    bool operator!=(const TQueue q);
+};
+
+#endif
